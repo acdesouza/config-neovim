11dc93e4394d7eb653ffd64695b2c7c0303841c6
Antonio Carlos da Graça Mota Durão de Souza <ac.desouza@gmail.com>
2024-01-05 18:28:29 -0300

    [UX] Show line number on current line. And distance to other lines.

---
+++ after/plugin/0_defaults.lua
@@ -0,0 +1,5 @@
+-- Filename is to make sure it will be executed before anything else. 
+
+-- Show line number on cursor's line. And distance from current line in others.
+vim.wo.number = true          --Make line numbers default
+vim.wo.relativenumber = true  --Make relative line numbers default
