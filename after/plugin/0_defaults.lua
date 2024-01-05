8be19ae9dc62afa64ebf67a70ccedc6728cd4038
Antonio Carlos da Graça Mota Durão de Souza <ac.desouza@gmail.com>
2024-01-05 18:55:03 -0300

    [UX] Replace tab with space. Set tab as 2 spaces.

--- after/plugin/0_defaults.lua
+++ after/plugin/0_defaults.lua
@@ -8,3 +8,9 @@ vim.wo.relativenumber = true  --Make relative line numbers default
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
 vim.o.clipboard = 'unnamedplus'
+
+-- Tabsize
+vim.o.tabstop     = 2 -- size of a hard tabstop (ts).
+vim.o.shiftwidth  = 2 -- size of an indentation (sw).
+vim.o.softtabstop = 2 -- number of spaces a <Tab> counts for. When 0, feature is off (sts).
+vim.o.expandtab = true -- always uses spaces instead of tab characters (et).
