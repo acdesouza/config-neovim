d4af5e61d86f8d0af6c7931061a19455b22bd93b
Antonio Carlos da Graça Mota Durão de Souza <ac.desouza@gmail.com>
2024-01-06 21:40:07 -0300

    [Plugin] vim-tmux-navigator - Move from Vim to Tmux panel with the same keybinding. Except when tmux Pane is zoomed.

---
+++ lua/plugins/vim-tmux-navigator.lua
@@ -0,0 +1,23 @@
+return {
+  "christoomey/vim-tmux-navigator",
+
+  config = function()
+    vim.g.tmux_navigator_disable_when_zoomed = 1
+  end,
+  
+  cmd = {
+    "TmuxNavigateLeft",
+    "TmuxNavigateDown",
+    "TmuxNavigateUp",
+    "TmuxNavigateRight",
+    "TmuxNavigatePrevious",
+  },
+  
+  keys = {
+    { "<C-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+    { "<C-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+    { "<C-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+    { "<C-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+    { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+  },
+}
